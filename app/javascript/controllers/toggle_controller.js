5d5f4c6ca887d5ce4487215ac200217571da31b4
Federico <federico.capaldo@hotmail.com>
2023-05-30 03:30:39 +0100

    added actual toggle functionality through stimulus on button click

--- app/javascript/controllers/toggle_controller.js
+++ app/javascript/controllers/toggle_controller.js
@@ -2,11 +2,13 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="toggle"
 export default class extends Controller {
+  static targets = ['togglableElement']
+
   connect() {
     console.log('Hello from toggle controller')
   }
 
   toggleNewMonumentForm() {
-    console.log("toggling...")
+    this.togglableElementTarget.classList.toggle("d-none")
   }
 }
